394f246d4c202ccbf4c4eb55085540f07d260b7a
Guillermo Fernandez <guillermo.fernandez@truata.com>
2025-09-06 20:56:56 +0300

    improving build script

---
+++ projects/cheesebytes-web/build.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+import shutil
+
+import sisifo
+
+CHEESEBYTES_PUBLIC_REPO_SSH = "git@github.com:guiferviz/cheesebytes.git"
+
+
+@sisifo.task
+def install_git_filter_repo():
+    if shutil.which("git-filter-repo"):
+        return sisifo.skip("git-filter-repo is already installed")
+    sisifo.shell("brew install git-filter-repo")
+
+
+@sisifo.task
+def publish_subtree_to_repo():
+    install_git_filter_repo()
+    dest_dir = "/tmp/cheesebytes-export"
+    export_paths = ["projects/cheesebytes-web", "'notes/Cheese Bytes'"]
+    # Commit: adding cheesebytes web pointing to notes under `notes/Cheese Bytes`
+    # Used to limit the export range and speed up the process.
+    first_commit = "d221709c7dcf999f0e4ad66bb857b5feadb9198d"
+
+    dest = Path(dest_dir)
+    dest.mkdir(parents=True, exist_ok=True)
+    if not (dest / ".git").exists():
+        sisifo.shell(f"cd {dest} && git init")
+
+    sisifo.shell(
+        f"git fast-export {first_commit}^..main -- {' '.join(export_paths)} | (cd {dest} && git fast-import)"
+    )
+    sisifo.shell(f"git -C {dest} checkout -f")
+    sisifo.shell(f"git -C {dest} branch -M main")
+    sisifo.shell(
+        f"git -C {dest} filter-repo --force --path-rename 'projects/cheesebytes-web/.github:.github'"
+    )
+    sisifo.shell(f"git -C {dest} remote add origin {CHEESEBYTES_PUBLIC_REPO_SSH}")
+
+
+if __name__ == "__main__":
+    sisifo.run_cli()
