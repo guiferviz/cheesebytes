25230c0ff8d8814b0f3927f190c800a1fc8569ed
guiferviz <guiferviz@gmail.com>
2026-01-14 20:42:58 +0200

    renaming aqui no hay quien duerma

--- apps/cheesebytes-web/src/components/cave/index.ts
+++ apps/cheesebytes-web/src/components/cave/index.ts
@@ -2,6 +2,6 @@
 // Cada subcarpeta tiene su propio index.ts para imports agrupados
 
 // Re-export subcarpetas
-export * from './nombres-elementales';
-export * from './aqui-no-hay-quien-duerma';
-export * from './shared';
+export * from "./nombres-elementales";
+export * from "./the-apartment-that-never-sleeps";
+export * from "./shared";
